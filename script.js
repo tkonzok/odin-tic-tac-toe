cc3863d46b7fa00fbf2f438625c9f25bdf97eba2
Tobias Konzok <konzok.tobias@gmail.com>
2023-07-10 10:03:28 +0200

    Add form for players names. Logic to be done.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function Gameboard() {
     const rows = 3;
     const columns = 3;
@@ -29,6 +31,41 @@ function Gameboard() {
         }
     }
 
+    const askForNames = () => {
+        const main = document.querySelector('main');
+        const div = document.createElement('div');
+        main.insertBefore(div, main.children[0]);
+
+        const form = document.createElement('form');
+        div.appendChild(form);
+
+        const input1 = document.createElement('input');
+        input1.type = 'text';
+        input1.id = 'playerOne';
+        input1.placeholder = 'Player 1';
+        form.appendChild(input1);
+
+        const input2 = document.createElement('input');
+        input2.type = 'text';
+        input2.id = 'playerTwo';
+        input2.placeholder = 'Player 2';
+        form.appendChild(input2);
+
+        const submitButton = document.createElement('button');
+        submitButton.type = 'submit';
+        submitButton.textContent = 'Create Players';
+        form.appendChild(submitButton);
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const playerOne = document.getElementById('playerOne');
+            const playerTwo = document.getElementById('playerTwo');
+            game.players[0]['name'] = playerOne;
+            game.players[1]['name'] = playerTwo;
+            main.removeChild(main.firstElementChild);
+        })
+    }
+
     const printBoard = () => {
         const boardValues = board.map((row) => row.map((cell) => cell.getValue()));
         for (let i = 0; i < 3; i++) {
@@ -38,13 +75,57 @@ function Gameboard() {
                 cell.innerHTML = boardValues[i][j];
             }
         }
+        const main = document.querySelector('main');
+        const div = document.createElement('div');
+        main.appendChild(div);
+
+        const playerOne = document.createElement('div');
+        playerOne.id = 'playerOneDiv';
+        div.appendChild(playerOne);
+
+        const playerTwo = document.createElement('div');
+        playerOne.id = 'playerTwoDiv';
+        div.appendChild(playerTwo);
+
+        const playerOneToken = document.createElement('p');
+        playerOneToken.id = 'playerOneToken';
+        playerOneToken.textContent = 'X';
+        playerOne.appendChild(playerOneToken);
+
+        const playerOneName = document.createElement('p');
+        playerOneName.id = 'playerOneName';
+        // playerOneName.textContent = game.players[0]['name'];
+        playerOneName.textContent = 'Player One';
+        playerOne.appendChild(playerOneName);
+
+        const playerOneScore = document.createElement('p');
+        playerOneScore.id = 'playerOneScore';
+        playerOneScore.textContent = 0;
+        playerOne.appendChild(playerOneScore);
+
+        const playerTwoToken = document.createElement('p');
+        playerTwoToken.id = 'playerTwoToken';
+        playerTwoToken.textContent = 'O';
+        playerTwo.appendChild(playerTwoToken);
+
+        const playerTwoName = document.createElement('p');
+        playerTwoName.id = 'playerTwoName';
+        // playerTwoName.textContent = game.players[1]['name'];
+        playerTwoName.textContent = 'Player Two';
+        playerTwo.appendChild(playerTwoName);
+        
+        const playerTwoScore = document.createElement('p');
+        playerTwoScore.id = 'playerTwoScore';
+        playerTwoScore.textContent = 0;
+        playerTwo.appendChild(playerTwoScore);
     }
 
     return {
         getBoard,
         dropToken,
         printBoard,
-        eventListener
+        eventListener,
+        askForNames
     }
 }
 
@@ -86,6 +167,7 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
     const getActivePlayer = () => activePlayer;
 
     const initGame = () => {
+        board.askForNames();
         board.eventListener();
         board.printBoard();
         console.log(`${getActivePlayer().name} begins.`)
